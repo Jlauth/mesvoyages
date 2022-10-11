e8094c2d46cbc5b98ea39296d8773eda379d9eaa
Jlauth <lauth_jean@live.fr>
2022-10-11 13:08:28 +0200

    Phase 19.1 : actualisation de AccueilController.php

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,20 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Description of AccueilController
- *
- * @author Jean
- */
-class AccueilController {
+
+ /**
+  * Description of AccueilController
+  * 
+  * @ Jean
+  */
+class AccueilController extends AbstractController{
+
+    
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
+    
     public function index(): Response{
-        return new Response('Plop!');
+        return $this->render("pages/accueil.html.twig");
     }
 }
