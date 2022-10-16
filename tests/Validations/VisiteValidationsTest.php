f51febbd7d7008d65dae18bebe40492ba97fb2c1
Jlauth <81298407+Jlauth@users.noreply.github.com>
2022-10-16 10:49:00 +0200

    Phase 24 : correction tests exercice.

--- tests/Validations/VisiteValidationsTest.php
+++ tests/Validations/VisiteValidationsTest.php
@@ -37,17 +37,58 @@ class VisiteValidationsTest extends KernelTestCase {
         $this->assertCount($nbErreursAttendues, $error, $message);
     }
     
-    
+    /**
+     * Test sur la validité d'une note
+     */
     public function testValidNoteVisite() {
-        $visite = $this->getVisite()->setNote(25);
-        $this->assertErrors($visite, 1);
+        $this->assertErrors($this->getVisite()->setNote(10), 0, "10 devrait réussir");
+        $this->assertErrors($this->getVisite()->setNote(0), 0, "0 devrait réussir");
+        $this->assertErrors($this->getVisite()->setNote(20), 20, "20 devrait réussir");
     }
     
-    public function testNonValidTempMax() {
-        $visite = $this->getVisite()
-            ->setTempmin(20)
-            ->setTempmax(18);
-        $this->assertErrors($visite, 1, "ceci est un message d'erreur");
-                    
+    /**
+     * Test sur la non validité d'une note
+     */
+    public function testNonValidNoteVisite() {
+        $this->assertErrors($this->getVisite()->setNote(21), 1, "21 devrait échouer");
+        $this->assertErrors($this->getVisite()->setNote(-1), 1, "-1 devrait échouer");
+        $this->assertErrors($this->getVisite()->setNote(-5), 1, "-5 devrait échouer");
+        $this->assertErrors($this->getVisite()->setNote(25), 1, "25 devrait échouer");
+    }
+    
+    /**
+     * Test sur la validité de la température maximale
+     */
+    public function testValidTempmaxVisite() {
+        $this->assertErrors($this->getVisite()->setTempmin(18)->setTempmax(20), 0, "min=18, max=20 devrait réussir");
+        $this->assertErrors($this->getVisite()->setTempmin(19)->setTempmax(20), 0, "min=19 max=20 devrait réussir");
+    }
+    
+    /**
+     * Test sur la non validité d'une température maximale
+     */
+    public function testNonValidTempMaxVisite() {
+        $this->assertErrors($this->getVisite()->setTempmin(20)->setTempmax(18), 1, "min=20 max=18 devrait échouer");
+        $this->assertErrors($this->getVisite()->setTempmin(20)->setTempmax(20), 1, "min=20 max=20 devrait échouer");                
+    }
+    
+    /**
+     * Test sur la validité de la date de création
+     */
+    public function testValidDatecreationVisite() {
+        $aujourdhui = new \DateTime();
+        $this->assertErrors($this->getVisite()->setDatecreation($aujourdhui), 1, "demain devrait échouer");
+        $plustot = (new \DateTime())->sub(new \DateInterval("P5D"));
+        $this->assertErrors($this->getVisite()->setDatecreation($plustot), 0, "plus tôt devrait réussir");
+    }
+    
+    /**
+     * Test sur la non validité de la date de création
+     */
+    public function testNonValidDatecreationVisite() {
+        $demain = (new DateTime())->add(new \DateInterval("P1D"));
+        $this->assertErrors($this->getVisite()->setDatecreation($demain), 1, "demain devrait échouer");
+        $plustard = (new DateTime())->add(new DateInterval("P5D"));
+        $this->assertErrors($this->getVisite()->setDatecreation($plustard), 1, "plus tard devrait échouer");
     }
 }
--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
 use DateTime;
 use PHPUnit\Framework\TestCase;
@@ -17,5 +18,16 @@ class VisiteTest extends TestCase
        $visite = new Visite();
        $visite->setDatecreation(new DateTime("2022-04-14"));
        $this->assertEquals("14/04/2022", $visite->getDatecreationString());
+   }
+   
+   public function testAddEnvironnement() {
+       $environnement = new Environnement();
+       $environnement->setNom("plage");
+       $visite = new Visite();
+       $visite->addEnvironnement($environnement);
+       $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+       $visite->addEnvironnement($environnement);
+       $nbEnvironnementApres = $visite->getEnvironnements()->count();
+       $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
    }
 }
